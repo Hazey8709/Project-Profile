3526deb9acb531a54a8e52d92236b8afdad1f166
Hazey8709 <jfortin802@gmail.com>
2023-07-18 23:29:09 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//?   Auto Type Method (index.html)
+
+var typed = new Typed(".autoInput", {
+    strings: ["Welcome to Jordan's Profile"],
+    typeSpeed: 150,
+    backSpeed: 80,
+    backDelay: 1500,
+    loop: true,
+});
