7f0af20e8756525ab868ce347a016386ad22f7c5
Hazey8709 <jfortin802@gmail.com>
2023-07-19 22:50:56 -0400

    updated home page content

--- js/script.js
+++ js/script.js
@@ -5,5 +5,126 @@ var typed = new Typed(".autoInput", {
     typeSpeed: 150,
     backSpeed: 80,
     backDelay: 1500,
-    loop: true,
+    loop: false,
 });
+
+//!   Stored from index.html  (Flip Cards)
+
+//  <div class="flip-cardCont">
+// <div class="flip-inner">
+//     <div class="flip-front">
+//         <img
+//             src=""
+//             alt="Resource"
+//             style="
+//                 width: 200px;
+//                 height: 200px;
+//                 border-radius: 10px;
+//             "
+//         />
+//     </div>
+//     <div class="flip-back">
+//         <h1>Sand Drawing</h1>
+//         <p>Tweety</p>
+//         <p>Cliff Walk RI</p>
+//     </div>
+// </div>
+// </div>
+// </div>
+
+// <div id="cardsCont">
+// <div class="flip-cardCont">
+// <div class="flip-inner">
+//     <div class="flip-front">
+//         <img
+//             src=""
+//             alt="Resource"
+//             style="
+//                 width: 200px;
+//                 height: 200px;
+//                 border-radius: 10px;
+//             "
+//         />
+//     </div>
+//     <div class="flip-back">
+//         <h1>Sand Drawing</h1>
+//         <p>Tweety</p>
+//         <p>Cliff Walk RI</p>
+//     </div>
+// </div>
+// </div>
+// </div>
+
+// <div id="cardsCont">
+// <div class="flip-cardCont">
+// <div class="flip-inner">
+//     <div class="flip-front">
+//         <img
+//             src=""
+//             alt="Resource"
+//             style="
+//                 width: 200px;
+//                 height: 200px;
+//                 border-radius: 10px;
+//             "
+//         />
+//     </div>
+//     <div class="flip-back">
+//         <h1>Sand Drawing</h1>
+//         <p>Tweety</p>
+//         <p>Cliff Walk RI</p>
+//     </div>
+// </div>
+// </div>
+// </div>
+
+
+//! /*!      Flip Cards          */
+// .flip-cardCont {
+//     border: 0.3rem inset black;
+//     border-radius: 1rem;
+//     margin: 0 auto;
+//     margin-top: 5rem;
+//     padding: 2rem;
+//     background-color: rgba(57, 56, 56, 0.333);
+//     width: 200px;
+//     height: 200px;
+//     perspective: 1000px;
+// }
+
+// .flip-inner {
+//     position: relative;
+//     border-radius: 1rem;
+//     margin: 0 auto;
+//     width: 100%;
+//     height: 100%;
+//     text-align: center;
+//     transition: transform 0.6s;
+//     transform-style: preserve-3d;
+//     box-shadow: 1px 1px 10px 1px white;
+// }
+
+// .flip-cardCont:hover .flip-inner {
+//     transform: rotateY(180deg);
+// }
+
+// .flip-front,
+// .flip-back {
+//     border-radius: 1rem;
+//     position: absolute;
+//     width: 100%;
+//     height: 100%;
+//     -webkit-backface-visibility: hidden;
+//     backface-visibility: hidden;
+// }
+
+// .flip-front {
+//     background-color: rgb(7, 243, 243);
+//     color: black;
+// }
+
+// .flip-back {
+//     background-color: rgba(0, 0, 0, 0.596);
+//     color: white;
+//     transform: rotateY(180deg);
+// }
